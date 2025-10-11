af062d5f1d1d8022ba1e7bd17b684347976b675d
Vince Resente <vresente@me.com>
2025-10-11 21:14:06 +0200

    xx

--- Sandbox/onprem-2-azure.sh
+++ Sandbox/onprem-2-azure.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# --------------------------------------------------------------------------------
+# #####--------------------------------------------------------------------------------
 # Script: setup-hybrid-network.sh
 # Purpose: Create a simulated hybrid network in Azure (single RG version):
 #          - "Hub" virtual network with VPN gateway
